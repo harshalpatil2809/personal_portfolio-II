78980eca65477e8bfde74823a938ffb3b07925a1
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-08-29 21:18:21 +0530

    Footer Done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import "./Footer.css"
 const Footer  = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <div className="footer">
+    <p className="footer1">Made with ❤️ by Harshal.</p>
+    <p>© 2025 Harshal Patil. All Rights Reserved.</p>
+</div>
   )
 }
 
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const Layout = () => {
   return (
     <>
       <Header/>
+
+      <Footer/>
     </>
   )
 }
